e792d2485636ddd0d45c1be6a9492c4d1055fc56
Scott Hillman <hillmanov@gmail.com>
2020-12-12 07:08:00 -0700

    Day 10 part 2
    Had to get major pointers from Jeff to complete this one. What a pain.

--- day10/day10.go
+++ day10/day10.go
@@ -14,10 +14,10 @@ func main() {
 
 	// Add the charging port and final adapter
 
-	// part1Solution := part1(jolts)
+	part1Solution := part1(jolts)
 	part2Solution := part2(jolts)
 
-	// fmt.Printf("Day 10: Part 1: = %+v\n", part1Solution)
+	fmt.Printf("Day 10: Part 1: = %+v\n", part1Solution)
 	fmt.Printf("Day 10: Part 2: = %+v\n", part2Solution)
 }
 
@@ -43,43 +43,26 @@ func part1(jolts []int) int {
 }
 
 func part2(jolts []int) int {
-	// Get all permutation ranges
+	jolts = append(jolts, 0)
+	jolts = append(jolts, utils.MaxOf(jolts)+3)
 
 	sort.Slice(jolts, func(i, j int) bool {
 		return jolts[i] < jolts[j]
 	})
 
-	fmt.Printf("jolts = %+v\n", jolts)
+	// No idea how or why this works. Taken from https://github.com/jhillman/advent-of-code-2020/blob/master/day10/part2.c
+	arrangements := make([]int, utils.MaxOf(jolts)+1)
+	arrangements[0] = 1
 
-	// jolts = [1 2 3 4 7 8 9 10 11 14 17 18 19 20 23 24 25 28 31 32 33 34 35 38 39 42 45 46 47 48 49]
-	options := make([]int, len(jolts))
+	for i := 0; i < len(jolts)-1; i++ {
+		joltage := jolts[i]
 
-	for i := 1; i < len(jolts); i++ {
-
+		for diff := 1; diff <= 3; diff++ {
+			if joltage-diff >= 0 {
+				arrangements[joltage] += arrangements[joltage-diff]
+			}
+		}
 	}
-	fmt.Printf("options = %+v\n", options)
 
-	return -1
+	return utils.MaxOf(arrangements)
 }
-
-/*
-
-0 1 4 5 6 7
-0 1 1 1 2 4
-
-*/
-
-// [0 1 4 5 6 7 10 11 12 15 16
-
-/*
-(0), 1, (4, 5, 6, 7), (10, 11, 12), 15, 16, 19, (22)
-(0), 1, (4, 5, 6, 7), (10,     12), 15, 16, 19, (22)
-
-(0), 1, (4, 5,    7), (10, 11, 12), 15, 16, 19, (22)
-(0), 1, (4,    6, 7), (10, 11, 12), 15, 16, 19, (22)
-(0), 1, (4,       7), (10, 11, 12), 15, 16, 19, (22)
-
-(0), 1, (4, 5,    7), (10,     12), 15, 16, 19, (22)
-(0), 1, (4,    6, 7), (10,     12), 15, 16, 19, (22)
-(0), 1, (4,       7), (10,     12), 15, 16, 19, (22)
-*/
