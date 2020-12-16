3aac375e7add0a97a378b23c38107fd15c44b858
Scott Hillman <hillmanov@gmail.com>
2020-12-16 05:43:12 -0700

    Day 16 part 1

---
+++ day16/day16.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"adventofcode/utils"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Rule struct {
+	Field  string
+	Ranges [2][2]int
+}
+
+type Ticket []int
+
+func (r Rule) isValid(v int) bool {
+	return v >= r.Ranges[0][0] && v <= r.Ranges[0][1] || v >= r.Ranges[1][0] && v <= r.Ranges[1][1]
+}
+
+func main() {
+	lines, err := utils.ReadLines("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	rules, _, nearbyTickets := parseInput(lines)
+
+	part1Solution := part1(rules, nearbyTickets)
+	part2Solution := part2(lines)
+
+	fmt.Printf("Day 16: Part 1: = %+v\n", part1Solution)
+	fmt.Printf("Day 16: Part 2: = %+v\n", part2Solution)
+}
+
+func part1(rules []Rule, nearbyTickets []Ticket) int {
+	invalidValues := []int{}
+
+	for _, nearbyTicket := range nearbyTickets {
+		for _, value := range nearbyTicket {
+			valueIsValid := false
+			for _, rule := range rules {
+				if rule.isValid(value) {
+					valueIsValid = true
+					break
+				}
+			}
+			if !valueIsValid {
+				invalidValues = append(invalidValues, value)
+			}
+		}
+	}
+
+	return utils.SumOf(invalidValues)
+}
+
+func part2(lines []string) int {
+	return -1
+}
+
+func parseInput(lines []string) (rules []Rule, yourTicket Ticket, nearbyTickets []Ticket) {
+	i := 0
+	// Parse rules
+	for ; len(lines[i]) > 0; i++ {
+		rule := Rule{}
+		parts := strings.Split(lines[i], ":")
+
+		rule.Field = strings.TrimSpace(parts[0])
+
+		var range1Low, range1High, range2Low, range2High int
+		fmt.Sscanf(parts[1], "%d-%d or %d-%d", &range1Low, &range1High, &range2Low, &range2High)
+		rule.Ranges = [2][2]int{{range1Low, range1High}, {range2Low, range2High}}
+
+		rules = append(rules, rule)
+	}
+
+	// Parse your ticket
+	i += 2
+	yourTicket = parseIntsFromLine(lines[i])
+
+	// Parse nearby tickets
+	i += 3
+	for ; i < len(lines); i++ {
+		nearbyTickets = append(nearbyTickets, parseIntsFromLine(lines[i]))
+	}
+
+	return rules, yourTicket, nearbyTickets
+}
+
+func parseIntsFromLine(line string) []int {
+	ints := strings.Split(line, ",")
+	values := make([]int, 0, len(ints))
+	for _, num := range ints {
+		v, _ := strconv.Atoi(num)
+		values = append(values, v)
+	}
+	return values
+}
