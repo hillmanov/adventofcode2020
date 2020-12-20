5793089b1da9a7619cb561464ec7865ffb4d4400
Scott Hillman <hillmanov@gmail.com>
2020-12-20 09:14:01 -0700

    Dy 18 part 1 and part 2
    Finally

--- day18/day18.go
+++ day18/day18.go
@@ -4,29 +4,23 @@ import (
 	"adventofcode/utils"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type any interface{}
 
 type token int
 
-func (t token) IsOperator() bool {
-	return t == Add || t == Multiply
-}
-
 const (
-	Space      token = 0
-	Number     token = 1
-	Add        token = 2
-	Multiply   token = 3
-	OpenParen  token = 4
-	CloseParen token = 5
+	Number     token = 0
+	Add        token = 1
+	Multiply   token = 2
+	OpenParen  token = 3
+	CloseParen token = 4
 )
 
 func tokenType(r rune) token {
 	switch r {
-	case ' ':
-		return Space
 	case '+':
 		return Add
 	case '*':
@@ -44,7 +38,7 @@ func main() {
 	expressions, _ := utils.ReadLines("./input.txt")
 
 	part1Solution := part1(expressions)
-	part2Solution := part2()
+	part2Solution := part2(expressions)
 
 	fmt.Printf("Day 18: Part 1: = %+v\n", part1Solution)
 	fmt.Printf("Day 18: Part 2: = %+v\n", part2Solution)
@@ -53,93 +47,108 @@ func main() {
 func part1(expressions []string) int {
 	sum := 0
 	for _, expression := range expressions {
-		sum += evaluateExpression(expression)
+		runes := []rune(strings.ReplaceAll(expression, " ", ""))
+		sum += evaluateExpression(&runes)
 	}
 	return sum
 }
 
-func part2() int {
-	return -1
+func part2(expressions []string) int {
+	sum := 0
+	for _, expression := range expressions {
+		runes := []rune(strings.ReplaceAll(expression, " ", ""))
+		sum += evaluateExpression2(&runes)
+	}
+	return sum
 }
 
-func evaluateExpression(e string) int {
+func evaluateExpression(e *[]rune) int {
 	stack := []any{}
 
-	for _, r := range e {
-		switch tokenType(r) {
-		case Number:
-			num, _ := strconv.Atoi(string(r))
-			stack = append(stack, num)
-			if len(stack) >= 3 {
-				if value, ok := peek(stack, 1).(rune); ok && tokenType(value).IsOperator() {
-					var operandB, operation, operandA any
-					operandB, stack = pop(stack)
-					operation, stack = pop(stack)
-					operandA, stack = pop(stack)
+	for len(*e) != 0 {
+		tok := shift(e)
 
-					switch tokenType(operation.(rune)) {
-					case Add:
-						stack = append(stack, operandA.(int)+operandB.(int))
-					case Multiply:
-						stack = append(stack, operandA.(int)*operandB.(int))
-					}
-				}
-			}
-
+		switch tokenType(tok) {
 		case OpenParen:
-			stack = append(stack, r)
-		case CloseParen:
-			var value any
-			stack = append(stack, r)
-			value, stack = popParenResult(stack)
+			stack = append(stack, evaluateExpression(e))
+		case Number:
+			value, _ := strconv.Atoi(string(tok))
 			stack = append(stack, value)
+		case Add, Multiply:
+			stack = append(stack, tok)
+		case CloseParen:
+			return stack[0].(int)
+		}
 
-			if len(stack) >= 3 {
-				if value, ok := peek(stack, 1).(rune); ok && tokenType(value).IsOperator() {
-					var operandB, operation, operandA any
-					operandB, stack = pop(stack)
-					operation, stack = pop(stack)
-					operandA, stack = pop(stack)
+		if len(stack) >= 3 {
+			paramA, operator, paramB := stack[0].(int), stack[1].(rune), stack[2].(int)
+			stack = nil
+			switch tokenType(operator) {
+			case Add:
+				stack = append(stack, paramA+paramB)
+			case Multiply:
+				stack = append(stack, paramA*paramB)
+			}
+		}
+	}
+	return stack[0].(int)
+}
 
-					switch tokenType(operation.(rune)) {
-					case Add:
-						stack = append(stack, operandA.(int)+operandB.(int))
-					case Multiply:
-						stack = append(stack, operandA.(int)*operandB.(int))
-					}
-				}
+func evaluateExpression2(e *[]rune) int {
+	stack := []any{}
+
+	done := false
+	for len(*e) != 0 && !done {
+		tok := shift(e)
+
+		switch tokenType(tok) {
+		case OpenParen:
+			stack = append(stack, evaluateExpression2(e))
+		case Number:
+			value, _ := strconv.Atoi(string(tok))
+			stack = append(stack, value)
+		case Add, Multiply:
+			stack = append(stack, tok)
+		case CloseParen:
+			done = true
+			break
+		}
+
+		// Handle additions only
+		if len(stack) >= 3 {
+			var paramA, paramB int
+			var operator rune
+
+			if v, ok := stack[len(stack)-3].(int); ok {
+				paramA = v
+			}
+			if v, ok := stack[len(stack)-2].(rune); ok {
+				operator = v
+			}
+			if v, ok := stack[len(stack)-1].(int); ok {
+				paramB = v
 			}
 
-		case Add, Multiply:
-			stack = append(stack, r)
-		case Space:
-			continue
+			if tokenType(operator) == Add {
+				stack = stack[:len(stack)-3]
+				stack = append(stack, paramA+paramB)
+			}
+		}
+	}
+
+	// Take care of multiplications
+	for len(stack) > 1 {
+		paramA, operator, paramB := stack[0].(int), stack[1].(rune), stack[2].(int)
+		if tokenType(operator) == Multiply {
+			stack = append([]any{paramA * paramB}, stack[3:]...)
 		}
 	}
 
 	return stack[0].(int)
 }
 
-func popParenResult(s []any) (any, []any) {
-	var value any
-	_, s = pop(s)
-	value, s = pop(s)
-	_, s = pop(s)
-	return value, s
-}
-
-func pop(s []any) (any, []any) {
-	value, newS := s[len(s)-1], s[:len(s)-1]
-	return value, newS
-}
-
-func popUntil(s []any, t token) []any {
-	for value, ok := s[len(s)-1].(rune); ok && tokenType(value) != t; {
-		pop(s)
-	}
-	return s
-}
-
-func peek(s []any, index int) any {
-	return s[len(s)-1-index]
+func shift(l *[]rune) rune {
+	r := (*l)[0]
+	*l = (*l)[1:]
+	return r
 }
